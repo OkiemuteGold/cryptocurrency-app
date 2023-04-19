480cf2e27946b0ce3a71cc982c59d65989bfcbe5
Okiemute Gold <goldokiemute@gmail.com>
2023-04-19 17:05:29 +0100

    used Routes instead of switch for router v6, added all components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
@@ -13,29 +13,20 @@ function App() {
             <div className="navbar">
                 <Navbar />
             </div>
-            {/* <div className="main">
+
+            <div className="main">
                 <Layout>
                     <div className="routes">
-                        <Switch>
-                            <Route exact path="/">
-                                <Homepage />
-                            </Route>
-                            <Route exact path="/exchanges">
-                                <Exchanges />
-                            </Route>
-                            <Route exact path="/cryptocurrencies">
-                                <Cryptocurrencies />
-                            </Route>
-                            <Route exact path="/crypto/:coinId">
-                                <CryptoDetails />
-                            </Route>
-                            <Route exact path="/news">
-                                <News />
-                            </Route>
-                        </Switch>
+                        <Routes>
+                            <Route path='/' element={<Homepage />} />
+                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News />} />
+                        </Routes>
                     </div>
                 </Layout>
-                
+
                 <div className="footer">
                     <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
                         <Link to="/">
@@ -49,7 +40,7 @@ function App() {
                         <Link to="/news">News</Link>
                     </Space>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
